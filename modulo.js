2875f3cadeaf6e364f6b253711739373796d7ac8
Christian Sestito <sestitochristian4@gmail.com>
2025-08-07 16:32:28 +0200

    Create and use CommonJS modules

---
+++ modulo.js
@@ -0,0 +1,5 @@
+function modulo(messaggio) {
+  console.log(`Questo è il messaggio esportato da script.js: ${messaggio}`);
+}
+
+module.exports = modulo;
---
+++ script.js
@@ -0,0 +1,3 @@
+const modulo = require('./modulo.js');
+
+modulo("Messaggio da modulo.js");
